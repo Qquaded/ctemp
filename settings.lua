67a6238a7d671a2a3e680a3cb6f038de3de7fa70
devcell <153192057+Qquaded@users.noreply.github.com>
2025-08-16 21:46:37 +0300

    Update and rename settings to settings.lua

---
+++ settings.lua
@@ -0,0 +1,8 @@
+
+return {
+	ShowInCommandBar = true,
+    StartWhenServerStart = true,
+	AddNewCategoryBasedOnName = false,
+ 	DoIApproveTheRobloxAndConstructTOS = "yeah!!",
+ 	PaymentToInstall = "GamepassId (rbxassetid)",
+}
